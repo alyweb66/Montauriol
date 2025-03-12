9bdf26329dd4aa05f5e99a231525bc939b1a41a0
alyweb66 <alyweb66@gmail.com>
2025-03-12 15:04:19 +0100

    refacto

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,11 @@
 "use client";
-import Image from "next/image";
-import { useEffect, useState } from "react";
+
 
 export default function Home() {
 
   return (
     <div>
       <h1>Welcome to Next.js!</h1>
-      <Image
-        src="/vercel.svg"
-        alt="Vercel Logo"
-        width={72}
-        height={16}
-      />
     </div>
   );
 }
